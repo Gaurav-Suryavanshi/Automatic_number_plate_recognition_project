dbd6c69ae9c14fa07acfa05a45a1952c7dc3483b
Gaurav Saini <93535210+Gaurav-Suryavanshi@users.noreply.github.com>
2023-09-13 10:04:37 +0530

    Update car.py

--- car.py
+++ car.py
@@ -7,9 +7,6 @@ with open('car-number-plate-delhi-vehicle-registration-license-india-european-st
         'https://api.platerecognizer.com/v1/plate-reader/',
         data=dict(regions=regions),  # Optional
         files=dict(upload=fp),
-        headers={'Authorization': 'Token 8833b7de8f16862bf2409c195ccee6febcaab01c'})
+        headers={'Authorization': 'Token 'YOUR_TOKEN'})
 plate_number = response.json()['results'][0]['plate']
 print("plate number is : " +plate_number.upper())
-
-
-
